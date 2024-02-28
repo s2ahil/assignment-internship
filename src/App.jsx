aa381d256b30c0c66f4499ebde9e73521303c5c9
s2ahil <101473078+s2ahil@users.noreply.github.com>
2024-02-28 22:04:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import { Appbar } from "./components/ManagePages.js"
+
 import Logo from "./asserts/logo.png"
 import Award from "./asserts/award.png"
 import GroupPhoto from "./asserts/grpPhoto.png"
@@ -11,7 +11,7 @@ function App() {
 
 
   return (
-    <>
+    <div className="bg-gradient-to-b from-yellow-50 via-white to-yellow-100 h-full">
       <div className='flex justify-center items-center  p-4'>
         <img src={Logo} className='h-[8rem]'></img></div>
 
@@ -50,28 +50,28 @@ function App() {
         </div>
 
         <div className='font-medium'>
-        <span className='border-r border-solid border-red-500 pr-4 mr-4'>CHEMICALS & PROCESS</span>
-  <span className='border-r border-solid  border-red-500  pr-4 mr-4'>POWER</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>WATER & WASTE WATER</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>OILS & GAS</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>PHARMA</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>SUGARS & DISTILLERIES</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>PAPER & PULP</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>MARINE & DEFENCE</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>METAL & MINING</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>FOOD & BEVERAGE</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>PETROCHEMICAL & REFINERIES</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>SOLAR</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>BUILDING HVAC</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>FIRE FIGHTING</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>AGRICULTURE & RESIDENTIAL</span>
+          <span className='border-r border-solid border-red-500 pr-4 mr-4'>CHEMICALS & PROCESS</span>
+          <span className='border-r border-solid  border-red-500  pr-4 mr-4'>POWER</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>WATER & WASTE WATER</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>OILS & GAS</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>PHARMA</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>SUGARS & DISTILLERIES</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>PAPER & PULP</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>MARINE & DEFENCE</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>METAL & MINING</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>FOOD & BEVERAGE</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>PETROCHEMICAL & REFINERIES</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>SOLAR</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>BUILDING HVAC</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>FIRE FIGHTING</span>
+          <span className='border-r border-solid  border-red-500 pr-4 mr-4'>AGRICULTURE & RESIDENTIAL</span>
         </div>
 
       </div>
 
 
       <div className='flex flex-col justify-center items-center md:flex-row md:justify-between gap-2 bg-red-500 h-[8rem] pl-4 pr-4 '>
-      <div className='flex gap-3 items-center text-white'>
+        <div className='flex gap-3 items-center text-white'>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
             <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
           </svg>
@@ -87,21 +87,21 @@ function App() {
 
         <div className='flex gap-3 items-center text-white  '>
 
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
-</svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+          </svg>
 
 
 
-<div>www.crigroups.com</div>
+          <div>www.crigroups.com</div>
 
-</div>
+        </div>
 
       </div>
 
-    
 
-    </>
+
+    </div>
   )
 }
 
