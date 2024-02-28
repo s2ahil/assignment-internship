7eef00d3124ee1cbfe8be859c40b5f73c8f121f8
s2ahil <101473078+s2ahil@users.noreply.github.com>
2024-02-28 21:52:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
         </div>
 
         <div className='font-medium'>
-        {/* <span className='border-r border-solid border-red-500 pr-4 mr-4'>CHEMICALS & PROCESS</span>
+        <span className='border-r border-solid border-red-500 pr-4 mr-4'>CHEMICALS & PROCESS</span>
   <span className='border-r border-solid  border-red-500  pr-4 mr-4'>POWER</span>
   <span className='border-r border-solid  border-red-500 pr-4 mr-4'>WATER & WASTE WATER</span>
   <span className='border-r border-solid  border-red-500 pr-4 mr-4'>OILS & GAS</span>
@@ -64,18 +64,20 @@ function App() {
   <span className='border-r border-solid  border-red-500 pr-4 mr-4'>SOLAR</span>
   <span className='border-r border-solid  border-red-500 pr-4 mr-4'>BUILDING HVAC</span>
   <span className='border-r border-solid  border-red-500 pr-4 mr-4'>FIRE FIGHTING</span>
-  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>AGRICULTURE & RESIDENTIAL</span> */}
+  <span className='border-r border-solid  border-red-500 pr-4 mr-4'>AGRICULTURE & RESIDENTIAL</span>
         </div>
 
       </div>
 
-      {/* <div className='flex  justify-between gap-2 bg-red-500 h-[5rem] pl-4 pr-4 w-full'>
-        <div className='flex gap-3 items-center text-white'>
+
+      <div className='flex flex-col justify-center items-center md:flex-row md:justify-between gap-2 bg-red-500 h-[8rem] pl-4 pr-4 '>
+      <div className='flex gap-3 items-center text-white'>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
             <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
           </svg>
           <div>Toll free 1800 200 1234</div>
         </div>
+
         <div className='flex gap-3 items-center text-white'>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
@@ -83,20 +85,21 @@ function App() {
           <div>www.facebook.com/cripumps</div>
         </div>
 
-
         <div className='flex gap-3 items-center text-white  '>
 
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
-          </svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+</svg>
 
 
 
-          <div>www.crigroups.com</div>
+<div>www.crigroups.com</div>
 
-        </div>
+</div>
 
-      </div> */}
+      </div>
+
+    
 
     </>
   )
